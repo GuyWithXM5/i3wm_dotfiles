77ab5ff0dc159fa6e5dbf0018e791ee31cca0f1c
guywithxm5 <anuvindap2016@gmail.com>
2026-03-01 18:08:25 +0530

    minor fix

--- .config/scripts/change_wallpaper.sh
+++ .config/scripts/change_wallpaper.sh
@@ -7,7 +7,7 @@ WALLPAPER="$1"
 wal -i "$WALLPAPER"
 
 # 3. Generate the blurred version for the background
-magick "$WALLPAPER" -resize 1920x1080^ -gravity center -extent 1920x1080 -blur 0x30 ~/.cache/blurred_wal.jpg
+magick "$WALLPAPER" -resize 1920x1080^ -gravity center -extent 1920x1080 -blur 0x30 ~/.cache/blurred_wall.jpg
 
 # 4. Set the actual wallpaper (using feh or your choice)
 feh --bg-fill "$WALLPAPER"
